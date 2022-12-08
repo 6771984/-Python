aefc325949ab59b2b77a1793ef771c7fdf2334b6
6771984 <119618318+6771984@users.noreply.github.com>
2022-12-08 11:52:07 +0300

    Update lesson_1_task_1.py

--- lesson_1_task_1.py
+++ lesson_1_task_1.py
@@ -2,7 +2,7 @@
 Запросите у пользователя некоторые числа и строки и сохраните в переменные,
 затем выведите на экран."""
 
-text = input ("Введите слово: ")
-letter_number = int(input ("Выберите номер буквы: "))-1
+text = input("Введите слово: ")
+letter_number = int(input("Выберите номер буквы: "))-1
 letter = text[letter_number]
-print (f"Буква: {letter}")
+print(f"Буква: {letter}")
