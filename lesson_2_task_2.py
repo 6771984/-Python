95b0f56e4c66f31d57d0971fcf2950be7f307b96
6771984 <119618318+6771984@users.noreply.github.com>
2022-12-16 22:33:31 +0300

    Update lesson_2_task_2.py

--- lesson_2_task_2.py
+++ lesson_2_task_2.py
@@ -7,7 +7,7 @@ my_list = input("Введите целые числа через пробел: "
 a = 0
 b = 1
 while len(my_list) > b:
-    my_list[a],my_list[b] = my_list[b],my_list[a]
+    my_list[a], my_list[b] = my_list[b], my_list[a]
     a += 2
     b += 2
 print('Результат:', *my_list)
