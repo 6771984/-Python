21b022a91b47c141d46e1366e6c6f7c67b502524
6771984 <119618318+6771984@users.noreply.github.com>
2023-02-17 22:53:44 +0300

    Update lesson_2_1_1.py

--- lesson_2_1_1.py
+++ lesson_2_1_1.py
@@ -1,7 +1,7 @@
-rub = input("Введите сумму в долларах: ")
-def doll (rub):
+eur = input("Введите сумму в долларах: ")
+def doll (eur):
     try:
-        return round(int(rub)/1.17,2)
+        return round(int(eur)/1.17,2)
     except ValueError:
         print("Недопустиое значение, вводите только числа")
-print(doll(rub))
+print(doll(eur))
